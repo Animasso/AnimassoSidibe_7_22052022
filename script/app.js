8320e9c6d8e04c752c2dde2872e8b8f5da28072c
Animasso Sidibe <asidibe017@gmail.com>
2022-06-10 02:12:48 +0200

    mise en place de l affichage des ingrdients dans la recette

--- script/app.js
+++ script/app.js
@@ -5,23 +5,53 @@
 async function fetchData() {
     const response = await fetch("./data/recipes.json")
     const data = await response.json()
-    return data.recipes;
+    return data.recipes
 }
-
 
 async function displayCard(recipes) {
     const allRecepies = document.getElementById("recipes-container-id");
+    const ulCard = document.querySelector('.ingredientsList')
     recipes.forEach((recipe) => {
-
         const recipesModel = recipesFactory(recipe);
         const recipesCardDOM = recipesModel.buildRecipesCard();
         allRecepies.appendChild(recipesCardDOM);
     });
 }
+
+function displayIngredients(recipes) {
+//recuperation de la liste des ingredients de chaque recette
+    let ingredientsCard = []; 
+    for (let i = 0; i < recipes.length; i++) {
+        ingredientsCard.push(recipes[i].ingredients);
+       // console.log(ingredientsCard);
+    }  
+const ingred= document.querySelectorAll('.ingredients')
+//console.log(ingred);
+let ingredientsList = []
+    for (let el in ingredientsCard) {
+        for (let j = 0; j < ingredientsCard[el].length; j++) {
+            let items = ingredientsCard[el][j].ingredient;
+            ingredientsList.push(items.toLowerCase());
+        };
+    }
+    //elimination des doublons dans la liste
+    var uniqueArr = [...new Set(ingredientsList)]
+   // console.log(uniqueArr);
+  /*  for (let k = 0; k < uniqueArr.length; k++) {
+        const element = array[k];
+        console.log(el);
+    }
+   uniqueArr.forEach(element => {
+       ingred.innerHTML =element
+   });
+   */
+  }
+ 
 async function init() {
     const recipes  = await fetchData();
     displayCard(recipes)
     console.log(recipes);
+    displayIngredients(recipes)
 
 }
 
--- script/factories/recipesCardDOM.js
+++ script/factories/recipesCardDOM.js
@@ -11,33 +11,49 @@ function recipesFactory(data) {
   } = data;
 
   function buildRecipesCard() {
+    let ingredientsCard = []; 
+    let dataIng = data.ingredients
+    console.log(dataIng);
+    for (let i = 0; i < dataIng.length; i++) {
+        if (dataIng[i].unit != undefined ) {
+            ingredientsCard.push(dataIng[i].ingredient + ": " + dataIng[i].quantity + " " + dataIng[i].unit)
+            console.log(ingredientsCard);
+        }else{   
+        ingredientsCard.push(dataIng[i].ingredient + ": " + dataIng[i].quantity )
+        }
+
+      
+    }  
+    
+    const childrenListItems = buildUserChildList(ingredientsCard)
     return $elementDomFactory(
       "div",
       { class: "recette" },
-      $elementDomFactory("div", { class: "recette-image" }),
+      $elementDomFactory("div", { class: "recette-img" }),
+      $elementDomFactory("div", { class: "recette-card" },
       $elementDomFactory(
         "div",
         { class: "title-time" },
         $elementDomFactory("p", { class: "title" }, name),
-        $elementDomFactory("p", { class: "minute" }, time.toString())
+        $elementDomFactory("p", { class: "minute" },
+        $elementDomFactory("i", { class: "fa-regular fa-clock" }), time.toString() +' '+`min`,)
+        
       ),
-      $elementDomFactory(
-        "div",
-        { class: "ingredients-instructions" },
-        $elementDomFactory(
-          "div",
-          { class: "ingredients" },
-        //  $elementDomFactory("li", { class: "ing" }, ingredients)
-        ),
-        $elementDomFactory(
-          "div",
-          { class: "instructions" },
-          $elementDomFactory("p", { class: "instruction-lign" }, description)
+      $elementDomFactory("div",{ class: "ingredients-instructions" },
+        $elementDomFactory("ul",{ class: "ingredientsList" }, ...childrenListItems ),
+        $elementDomFactory( "div",{ class: "instructions" },
+        $elementDomFactory("p", { class: "instruction-lign" }, description)
         )
       )
-    );
+    ));
+   
   }
-
+  function buildUserChildList(children) {
+    return children.map(child => ($elementDomFactory('li', {}, child )));
+  
+  }
+  
+ 
   return {
     id,
     appliance,
@@ -48,6 +64,7 @@ function recipesFactory(data) {
     time,
     ustensils,
     buildRecipesCard,
+    buildUserChildList
   };
 }
 
