69c6dad8fcb7a418dd32df45eb0eec5cb282cbb8
Animasso Sidibe <asidibe017@gmail.com>
2022-07-02 01:57:43 +0200

    ajout flavicon

---
+++ script/factories/tagsFactories.js
@@ -0,0 +1,49 @@
+function tagsFactory(){
+    const {
+        id,
+        appliance,
+        description,
+        ingredients,
+        name,
+        servings,
+        time,
+        ustensils,
+      } = data;
+
+function buildTagsCard(){
+    return $elementDomFactory(
+
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      return {
+        id,
+        appliance,
+        description,
+        ingredients,
+        name,
+        servings,
+        time,
+        ustensils,
+        buildTagsCard
+      
+      };
+}
